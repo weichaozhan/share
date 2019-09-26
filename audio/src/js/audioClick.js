22f38c689b92f9eb9a5025066e291168c7b69790
wczhan <921391554@qq.com>
2019-09-26 13:30:34 +0800

    修改

--- audio/src/js/audioClick.js
+++ audio/src/js/audioClick.js
@@ -1,4 +1,4 @@
-import { ARRFREQUENCY, TIGERS, } from './constant';
+import { ARRFREQUENCY, TIGERSCLICK, } from './constant';
 
 const btn = document.getElementById('sound-click');
 let index = 0;
@@ -6,21 +6,21 @@ let index = 0;
 var audioCtx = new AudioContext();
 
 btn.addEventListener('click', () => {
-  console.log(index, ARRFREQUENCY[TIGERS[index]]);
+  console.log(index, ARRFREQUENCY[TIGERSCLICK[index]]);
   
   var oscillator = audioCtx.createOscillator();
   var gainNode = audioCtx.createGain();
   oscillator.connect(gainNode);
   gainNode.connect(audioCtx.destination);
   oscillator.type = 'sine'; // 'sine', 'square', 'triangle', 'sawtooth'，自定义setPeriodicWave
-  oscillator.frequency.value = ARRFREQUENCY[TIGERS[index]];
+  oscillator.frequency.value = ARRFREQUENCY[TIGERSCLICK[index]];
   gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
   gainNode.gain.linearRampToValueAtTime(1, audioCtx.currentTime + 0.01);
   oscillator.start(audioCtx.currentTime);
   gainNode.gain.exponentialRampToValueAtTime(0.001, audioCtx.currentTime + 1);
   oscillator.stop(audioCtx.currentTime + 1);
   
-  if (index < TIGERS.length - 1) {
+  if (index < TIGERSCLICK.length - 1) {
     index++;
   } else {
     index = 0;
